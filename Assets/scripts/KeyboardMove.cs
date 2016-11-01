63257a8872514b09dfc94c59641168b370438b32
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2016-11-01 10:02:58 +0200

    add keyboard movement

---
+++ Assets/scripts/KeyboardMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardMove : MonoBehaviour {
+
+	public float speed = 5.0f;
+	void Start () {
+	
+	}
+
+	void Update () {
+		Vector3 move = new Vector3 ();
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			move = transform.forward;
+		}
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			move = -transform.forward;
+		}
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			move = transform.right;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			move = -transform.right;
+		}
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
