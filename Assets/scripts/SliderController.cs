95f1e492a38f299bfaaad7b34fefe8c06fc22866
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2016-11-01 12:29:20 +0200

    working sliders

---
+++ Assets/scripts/SliderController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MovePlatforms () {
+		foreach(Transform child in transform) {
+			if (Mathf.Abs (child.position.x) < 15) {
+				float newX = child.position.x * 5;
+				child.position = new Vector3 (newX, child.position.y, child.position.z);
+			}
+		}
+	}
+}
